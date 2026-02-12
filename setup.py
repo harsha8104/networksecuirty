ff8fdf5588eb4fa24d3a9ec583be3ab7c4be6490
MAHANANDIGARI HARSHA VARDHAN REDDY <harshavardhanr576@gmail.com>
2026-02-12 20:27:43 +0530

    Setup file ready

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements()->List[str]:
+    requirement_lst:list[str] = []
+    try:
+        with open('requirements.txt','r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("Requirements.txt file not found")
+    
+    return requirement_lst
+print(get_requirements())
+
+setup(
+    name = 'NetworkSecurity',
+    version = '0.0.1',
+    author = 'Harshavardhan',
+    author_email = 'harshavardhanr576@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
